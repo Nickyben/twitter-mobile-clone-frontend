f7687c203d659af7fb309ac9ccc4f47cf5ae5229
Nicholas Ikechukwu <nickyikechukwu@gmail.com>
2022-05-24 09:11:20 +0100

    Setup first commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/*global module*/
+/*eslint no-undef: "error"*/
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
